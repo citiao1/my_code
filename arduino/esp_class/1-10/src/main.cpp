e8c5b99394654c1419a185f77f2e3adf7636bfe6
citiao1 <2809796336@qq.com>
2026-03-16 21:57:04 +0800

    物联网第一次课

--- arduino/esp_class/1-10/src/main.cpp
+++ arduino/esp_class/1-10/src/main.cpp
@@ -4,6 +4,45 @@
 
 Adafruit_NeoPixel pixels=Adafruit_NeoPixel(4,13,NEO_GRB+NEO_KHZ800);
 
+long time1=0;
+long key_time=0;
+long led_time=0;
+int led_state=0;
+int led_flag=0;
+uint32_t colors[4]{
+  pixels.Color(255,0,0),
+  pixels.Color(0,255,0),
+  pixels.Color(0,0,255),
+  pixels.Color(255,255,255)
+};
+
+int led_color_index[4]={0,1,2,3};
+
+void key_pro(){
+  time1=millis();
+  if(time1-key_time<10)return;
+  key_time=time1;
+  if(digitalRead(4)==HIGH)led_state=1;
+  if(digitalRead(5)==LOW)led_state=0;
+}
+void led_app(){
+    time1=millis();
+    if(time1-led_time<2000)return;
+    led_time=time1;
+    pixels.clear();
+    pixels.setBrightness(255);
+    pixels.setPixelColor(led_flag,colors[led_color_index[led_flag]]);
+    pixels.show();
+    led_color_index[led_flag]++;
+    if(led_color_index[led_flag]>=4)led_color_index[led_flag]=0;
+    led_flag++;
+    if(led_flag>3)led_flag=0;
+    
+
+    
+  }
+  
+
 
 void setup() {
   pinMode(13,OUTPUT);
@@ -13,42 +52,8 @@ void setup() {
 }
 
 void loop() {
-  pixels.setBrightness(255);
-    for(int i=0;i<4;i++){
-    switch (i)
-    {
-    case 0:
-      pixels.setPixelColor(0,255,0,0);
-      pixels.setPixelColor(1,0,255,0);
-      pixels.setPixelColor(2,0,0,255);
-      pixels.setPixelColor(3,255,255,255);
-      break;
-    case 1:
-      pixels.setPixelColor(1,255,0,0);
-      pixels.setPixelColor(2,0,255,0);
-      pixels.setPixelColor(3,0,0,255);
-      pixels.setPixelColor(0,255,255,255);
-    break;
-    case 2:
-      pixels.setPixelColor(2,255,0,0);
-      pixels.setPixelColor(3,0,255,0);
-      pixels.setPixelColor(0,0,0,255);
-      pixels.setPixelColor(1,255,255,255);
-      break;
-    case 3:
-      pixels.setPixelColor(3,255,0,0);
-      pixels.setPixelColor(2,0,255,0);
-      pixels.setPixelColor(1,0,0,255);
-      pixels.setPixelColor(0,255,255,255);
-      break;
-    default:
-      break;
-    }
-    pixels.show();
-    
-
-    delay(2000);
-  }
-  pixels.show();
+  key_pro();
+  if(led_state==1)led_app();
+  
 }
 
