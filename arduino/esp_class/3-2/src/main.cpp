eabbc111c89bde4e44943ee0c3f4e7880900a1dd
citiao1 <2809796336@qq.com>
2026-03-18 18:31:26 +0800

    舵机

---
+++ arduino/esp_class/3-2/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <Servo.h>
+// put function declarations here:
+Servo myservo;
+int angle=0;
+
+void setup() {
+  myservo.attach(4);
+  myservo.write(0);
+  Serial.begin(115200);
+}
+
+void loop() {
+  angle=analogRead(A0);
+  angle=map(angle,0,1023,0,180);
+  myservo.write(angle);
+  Serial.println(angle);
+  delay(50);
+}
+
+
